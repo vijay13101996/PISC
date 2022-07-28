6ade7f1ef652e0fe908e9c239a78ea7a2fe28e6b
Yair Litman <yairlitman@gmail.com>
2022-07-28 09:52:20 +0100

    add all potentials to init file

--- PISC/potentials/__init__.py
+++ PISC/potentials/__init__.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+from PISC.potentials.double_well_potential import double_well
+
+from PISC.potentials.Adams_function  import adams_function
+from PISC.potentials.Coupled_harmonic import coupled_harmonic
+from PISC.potentials.double_well_potential import double_well
+from PISC.potentials.harmonic_1D import harmonic as harmonic1D
+from PISC.potentials.harmonic_2D import Harmonic as harmonic2D
+from PISC.potentials.Heller_Davis import  heller_davis
+from PISC.potentials.Henon_Heiles import  henon_heiles
+from PISC.potentials.Matsubara_M3_Quartic import  Matsubara_Quartic
+from PISC.potentials.Morse_1D import  morse
+from PISC.potentials.Quartic_bistable import  quartic_bistable
+from PISC.potentials.Quartic import quartic
+from PISC.potentials.trunc_harmonic import  trunc_harmonic
