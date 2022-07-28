07c941f854dac3e35e70ee5d8ccbaa9b73e0c910
Yair Litman <yairlitman@gmail.com>
2022-07-28 09:52:49 +0100

    add harmonic potential

--- PISC/potentials/harmonic_1D.py
+++ PISC/potentials/harmonic_1D.py
@@ -8,15 +8,12 @@ class harmonic(PES):
 		
 	def bind(self,ens,rp):
 		super(harmonic,self).bind(ens,rp)
-		self.ddpot_cart=self.ddpotmat*(self.rp.m3*self.omega**2)[:,:,None,:,None]
-	 	
-	def compute_potential(self):
-		self.pot = (self.omega**2/2.0)*self.rp.m3*self.rp.qcart**2 
-		return self.pot
 	
-	def compute_force(self):
-		self.dpot_cart = (self.rp.m3*self.omega**2)*self.rp.qcart 
-		return self.dpot_cart
+	def potential(self,q):
+		return  (self.omega**2/2.0)*q**2
+	
+	def dpotential(self,q):
+		return (self.omega**2)*self.q
 
-	def compute_hessian(self):
-		return self.ddpot_cart
+	def ddpotential(self,q):
+		return self.omega**2
