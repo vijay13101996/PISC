e0243677e1059c0e7886eb49fe6af5c905d311e6
Lars <meuserl@student.ethz.ch>
2022-09-06 17:08:20 +0100

    going back to previous commit

---
+++ Programs_Lars/mylib/plt_util.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt 
+import matplotlib
+
+
+font = {'family':'serif','size':12}
+
+std_width = 20
+std_height = 10
+std_dpi = 150
+file_dpi = 1200
+
+in_in_cm = 0.393701
+def prepare_fig(width=std_width, height=std_height,tex=False):
+    fig_width = width*in_in_cm
+    fig_height = height*in_in_cm
+    matplotlib.rc('font', **font)
+    if tex:
+        matplotlib.rc('text', usetex=True)
+    fig = plt.figure(num=None, figsize=(fig_width, fig_height),dpi=std_dpi, facecolor='w', edgecolor='k')
+    return fig
+def prepare_fig_ax(dim=1,width=std_width, height=std_height,tex=False,sharex=False):
+    fig_width = width*in_in_cm
+    fig_height = height*in_in_cm
+    matplotlib.rc('font', **font)
+    if tex:
+        matplotlib.rc('text', usetex=True)
+    if(sharex):
+        fig,ax = plt.subplots(1,dim,sharex=True, figsize=(fig_width, fig_height),dpi=std_dpi, facecolor='w', edgecolor='k')
+    else:
+        fig,ax = plt.subplots(1,dim, figsize=(fig_width, fig_height),dpi=std_dpi, facecolor='w', edgecolor='k')
+    return fig,ax
---
+++ expl_plotting.py
@@ -0,0 +1,8 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib import pyplot as plt
+x=range(10)
+y=range(10)
+plt.plot(x,y)
+plt.show()
