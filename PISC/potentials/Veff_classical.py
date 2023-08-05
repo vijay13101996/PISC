e98f30e94f0e1a9d4f48171f6551af873fc030b2
Vijay Ganesh Sadhasivam <vgs23@selene>
2023-08-05 23:42:53 +0100

    Minor error fixed in Veff_classical.py

--- PISC/potentials/Veff_classical.py
+++ PISC/potentials/Veff_classical.py
@@ -2,55 +2,60 @@ import numpy as np
 from PISC.potentials.base import PES
 
 class Veff_classical_1D_LH(PES):
-	"""
-	The local harmonic ansatz for the effective classical potential
-	"""
+		"""
+		The local harmonic ansatz for the effective classical potential
+		"""
 
-	def __init__(self,pes,beta,m,grad=None,hess=None):
-		super(Veff_classical_1D_LH).__init__()	
-		self.beta = beta 
-		self.m=m
-		self.pes = pes
-		self.grad = grad
-		self.hess = hess
-		
-	def bind(self,ens,rp):
-		super(Veff_classical_1D_LH,self).bind(ens,rp)
-		
-	def potential(self,q):
-		#This provides an option to pass a customised hessian field if required
-		if(self.hess is None):
-			hess = self.pes.ddpotential(q)/self.m
-		else:
-			hess = self.hess(q)/self.m
+		def __init__(self,pes,beta,m,grad=None,hess=None):
+				super(Veff_classical_1D_LH).__init__()
+				self.beta = beta
+				self.m=m
+				self.pes = pes
+				self.grad = grad
+				self.hess = hess
 
-		if(self.grad is None):
-			grad = self.dpotential(q)/self.m
-		else:
-			grad = self.grad(q)/self.m
-		
-		wa=0.0
-		ka = 2*grad	
-		Vc = self.pes.potential(q)
-		if (hess >0):
-			wa = np.sqrt(hess)
-			xi = 0.5*self.beta*wa
-			Vnc = m*ka**2*(np.tanh(xi)/xi -1)/(8*hess) - 0.5*np.log(np.tanh(xi)/xi)/self.beta + np.log(np.sinh(xi)/xi)/self.beta		
-			return Vc+Vnc	
-		elif (hess <0): 	
-			wa = np.sqrt(-hess)
-			xi = 0.5*self.beta*wa
-			# The potential is defined in terms of tan instead of tanh when the hessian is negative.
-			Vnc = m*ka**2*(np.tan(xi)/xi-1)/(8*hess) - 0.5*np.log(np.tan(xi)/xi)/self.beta + np.log(np.sin(xi)/xi)/self.beta
-			return Vc + Vnc
-		elif (hess==0):
-			return self.pes.potential(q)
-	
-	def dpotential(self,q):
-		return None
+		def bind(self,ens,rp):
+				super(Veff_classical_1D_LH,self).bind(ens,rp)
 
-	def ddpotential(self,q):
-		return None
+		def potential(self,q):
+				#This provides an option to pass a customised hessian field if required
+				if(self.hess is None):
+						hess = self.pes.ddpotential(q)/self.m
+				else:
+						hess = self.hess(q)/self.m
+
+				if(self.grad is None):
+						grad = self.pes.dpotential(q)/self.m
+				else:
+						grad = self.grad(q)/self.m
+	   
+				wa=0.0
+				ka = 2*grad
+				Vc = self.pes.potential(q)
+				if (hess >0):
+						wa = np.sqrt(hess)
+						xi = 0.5*self.beta*wa
+						Vnc = self.m*ka**2*(np.tanh(xi)/xi -1)/(8*hess) #+ 0.5*np.log(np.sinh(2*xi)/(2*xi))/self.beta
+						#- 0.5*np.log(np.tanh(xi)/xi)/self.beta + np.log(np.sinh(xi)/xi)/self.beta			  
+						return Vc+Vnc
+				elif (hess <0):
+						wa = np.sqrt(-hess)
+						xi = 0.5*self.beta*wa
+						#print('q,sinxi/xi',q, np.sin(2*xi)/(2*xi),np.sin(2*xi),2*xi)
+						# The potential is defined in terms of tan instead of tanh when the hessian is negative.
+						Vnc = self.m*ka**2*(np.tan(xi)/xi-1)/(8*hess) + 0.5*np.log(np.sin(2*xi)/(2*xi))/self.beta
+						#if(Vnc<-0.25):
+						#	 print('q,Vnc, Veff', np.tan(xi)/xi, np.around(q,2),np.around(Vnc,3),Vc+Vnc)
+						#- 0.5*np.log(np.tan(xi)/xi)/self.beta + np.log(np.sin(xi)/xi)/self.beta
+						return Vc + Vnc
+				elif (hess==0):
+						return Vc
+
+		def dpotential(self,q):
+				return None
+
+		def ddpotential(self,q):
+				return None
 
 class Veff_classical_1D_GH(PES):
 	"""
@@ -157,7 +162,7 @@ class Veff_classical_1D_FK(PES):
 			xi = 0.5*self.beta*wa
 			# Eq. 3.36 in text
 			return self.pes.potential(q) + np.log(np.sinh(xi)/xi)/self.beta
-		elif (hess <0): 	
+		elif (hess <0):		
 			wa = np.sqrt(-hess)
 			xi = 0.5*self.beta*wa
 			# Potential modified for negative hessian
